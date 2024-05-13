6d24f14430c356a59276c1c94e8b40a7b5df6c26
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2024-05-13 20:07:22 -0300

    update

--- teste.py
+++ teste.py
@@ -1,10 +1,18 @@
 saldo = 500
+print(saldo)
 saldo += 200
+print(saldo)
 saldo -= 100
+print(saldo)
 saldo *= 2
+print(saldo)
 saldo /= 4
+print(saldo)
 saldo //= 5
+print(saldo)
 saldo **= 3
+print(saldo)
 saldo %= 299
+print(saldo)
 saldo **= 3
 print(saldo)
