c65667f1a49b05f5b5992dbe4c9e7c26096cf9ad
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2024-06-20 17:50:20 -0300

    Update_Sistema_bacario_final

--- ex002/sistema_bancario.py
+++ ex002/sistema_bancario.py
@@ -33,7 +33,7 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
     excedeu_saldo = valor > saldo
     excedeu_limite = valor > limite
     excedeu_saques = numero_saques >= limite_saques
-    
+
     if excedeu_saldo:
         print("\n\033[0;30;41m Operação falhou! Você não tem saldo suficiente. \033[m")
     
@@ -47,7 +47,6 @@ def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
         saldo -= valor
         extrato += f"Saque: \t\t R$ {valor:.2f}\n"
         numero_saques += 1
-        #print(numero_saques)
         print("\n\033[0;30;42m Saque realizado com sucesso! \033[m")
         
     else:
@@ -64,16 +63,44 @@ def exibir_extrato(saldo, /, *, extrato):
 
 
 def criar_usuario(usuarios):
-    ""
+    cpf = int(input("Informe o CPF (somente números): "))
+    usuario = filtrar_usuario(cpf, usuarios)
 
-def filtrar_usuario(cpf, usuario):
-    ""
+    if usuario:
+        print("\n\033[0;30;41m Já existe usúario com esse CPF! \033[m")
+        return
+    nome = input(str("informe o nome completo: "))
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input("Informe o endereço (logradura, nru - bairro - cidade/sigla estado): ")
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco} )
+
+    print("\n\033[0;30;42m Usuário criado com sucesso! \033[m")
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
 def criar_conta(agencia, numero_conta, usuarios):
-    ""
+    cpf = int(input("Informe o CPF do usuário: "))
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n\033[0;30;42m Conta criada com sucesso! \033[m")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario }
+        
+    print("\n\033[0;30;41m Usuário não encontrado, fluxo de criação de conta encerrado! \033[m")
 
 def listar_contas(contas):
-    ""
+    for conta in contas:
+        linha = f"""\
+        Agência:\t{conta['agencia']}
+        C/C:\t\t{conta['numero_conta']}
+        Titular:\t{conta['usuario']['nome']}
+        """
+
+        print("=" * 10)
+        print(textwrap.dedent(linha))
 
 def main():
     LIMITE_SAQUES = 3
@@ -117,7 +144,7 @@ def main():
             conta = criar_conta(AGENCIA, numero_conta, usuarios)
 
             if conta:
-                conta.append(conta)
+                contas.append(conta)
 
         elif opcao == "lc":
             listar_contas(contas)
