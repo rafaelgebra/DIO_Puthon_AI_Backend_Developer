b63db6d0ea1da8650e9083988263d2519383803b
Rafael Rodrigues Gebra <rafaelrodriguesgebra@gmail.com>
2024-07-03 09:43:03 -0300

    Criando_exemplo_herança-simples

---
+++ introducao_a_(poo)/01_heranca_simples.py
@@ -0,0 +1,42 @@
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_placa = numero_rodas
+
+    def ligar_motor(sekf):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return self.cor
+
+class Motocicleta(Veiculo):
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f" {'Sim' if self.carregado else 'Não'} estou carregado")
+
+
+
+#moto = Motocicleta("preta", "abc-1234", "2")
+#moto.ligar_motor()
+
+#carro = Carro("branco", "xde-0000", 4)
+#carro.ligar_motor()
+#carro.esta_carregado()
+
+
+caminhao = caminhao("roxo", "asd-2423", 8, True)
+caminhao.ligar_motor()
+caminhao.esta_carregado()
+print(caminhao)
